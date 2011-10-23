9b7447e8c0f9cea4136b172234b3bc7ed34cd12d
JJ Graham <thetwoj@gmail.com>
2011-10-22 22:35:09 -0700

    Hello World test app

--- src/com/osu/sc/meadows/Meadows_CapstoneActivity.java
+++ src/com/osu/sc/meadows/Meadows_CapstoneActivity.java
@@ -2,12 +2,15 @@ package com.osu.sc.meadows;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Meadows_CapstoneActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
     }
 }
