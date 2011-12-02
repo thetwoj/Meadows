df10250ed6e39003adf424632e77a8e7da209eff
JJ Graham <thetwoj@gmail.com>
2011-12-01 16:33:26 -0800

    Commiting minor changes to strings.xml

--- src/com/osu/sc/meadows/StatsActivity.java
+++ src/com/osu/sc/meadows/StatsActivity.java
@@ -1,7 +1,9 @@
 package com.osu.sc.meadows;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
@@ -19,9 +21,12 @@ import com.google.android.apps.mytracks.services.ITrackRecordingService;
 /*
  * Place holder activity for eventual Stats features
  */
-public class StatsActivity extends Activity {
+public class StatsActivity extends Activity 
+{
 
 	private static final String TAG = StatsActivity.class.getSimpleName();
+
+	Boolean recording = false;
 
 	// utils to access the MyTracks content provider
 	private MyTracksProviderUtils myTracksProviderUtils;
@@ -29,7 +34,7 @@ public class StatsActivity extends Activity {
 	// display output from the MyTracks content provider
 	private TextView outputTextView;
 	private TextView averagespeed;
-	private TextView maxspeed;
+	private TextView totaldistance;
 	private TextView totaltime;
 	private TextView movingtime;
 
@@ -40,28 +45,34 @@ public class StatsActivity extends Activity {
 	private Intent intent;
 
 	// connection to the MyTracks service
-	private ServiceConnection serviceConnection = new ServiceConnection() {
+	private ServiceConnection serviceConnection = new ServiceConnection() 
+	{
 		@Override
-		public void onServiceConnected(ComponentName className, IBinder service) {
+		public void onServiceConnected(ComponentName className, IBinder service) 
+		{
 			myTracksService = ITrackRecordingService.Stub.asInterface(service);
 		}
 
 		@Override
-		public void onServiceDisconnected(ComponentName className) {
+		public void onServiceDisconnected(ComponentName className) 
+		{
 			myTracksService = null;
 		}
 	};
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.statslayout);
 
 		// for the MyTracks content provider
 		myTracksProviderUtils = MyTracksProviderUtils.Factory.get(this);
+
+		// Various text views for displaying statistics
 		outputTextView = (TextView) findViewById(R.id.output);
 		averagespeed = (TextView) findViewById(R.id.averagespeed);
-		maxspeed = (TextView) findViewById(R.id.maxspeed);
+		totaldistance = (TextView) findViewById(R.id.totaldistance);
 		totaltime = (TextView) findViewById(R.id.totaltime);
 		movingtime = (TextView) findViewById(R.id.movingtime);
 
@@ -71,55 +82,117 @@ public class StatsActivity extends Activity {
 				getString(R.string.mytracks_service_package), getString(R.string.mytracks_service_class));
 		intent.setComponent(componentName);
 
-		Button startRecordingButton = (Button) findViewById(R.id.start_recording_button);
-		startRecordingButton.setOnClickListener(new View.OnClickListener() {
+		final Button recordingButton = (Button) findViewById(R.id.recording_button);
+		recordingButton.setOnClickListener(new View.OnClickListener() 
+		{
 			@Override
-			public void onClick(View v) {
-				if (myTracksService != null) {
-					try {
-						myTracksService.startNewTrack();
-					} catch (RemoteException e) {
-						Log.e(TAG, "RemoteException", e);
+			public void onClick(View v) 
+			{
+				// If not currently recording
+				if (recording == false)
+				{
+					// If myTracks is connected successfully
+					if (myTracksService != null) 
+					{
+						// Try to start a new track
+						try {
+							myTracksService.startNewTrack();
+							
+							// Change recording bool to indicate track is recording
+							recording = true;
+							
+							// Set text from "Start" to "Stop"
+							recordingButton.setText("Stop");
+						} 
+						catch (RemoteException e) 
+						{
+							Log.e(TAG, "RemoteException", e);
+						}
 					}
+				} 
+				// If currently recording
+				else 
+				{
+					// If myTracks is connected successfully
+					if (myTracksService != null) 
+					{
+						// Try to stop the currently recording track
+						try {
+							myTracksService.endCurrentTrack();
+						} catch (RemoteException e) {
+							Log.e(TAG, "RemoteException", e);
+						}
+					}
+
+					// use the MyTracks content provider to get all the tracks
+					Track tracks = myTracksProviderUtils.getLastTrack();
+					outputTextView.setText(tracks.getStatistics() + "");
+
+					/* Set the retrieved statistics to the output TextView 
+					 * (for debugging/verify, will be removed)
+					 */
+					String data = outputTextView.getText().toString();
+
+					// Scrape the statistics for the corresponding field
+					String mtime = Scrape(data.indexOf("Moving Time:") + 13, data);
+					String ttime = Scrape(data.indexOf("Total Time:") + 12, data);
+					String aspeed = Scrape(data.indexOf("Average Speed:") + 15, data);
+					String tdistance = Scrape(data.indexOf("Total Distance:") + 16, data);
+
+					// Set the statistics to display in the appropriate locations
+					movingtime.setText(mtime);
+					totaltime.setText(ttime);
+					averagespeed.setText(aspeed);
+					totaldistance.setText(tdistance);
+
+					//movingtime.setText(Scrape(data.indexOf("Moving Time" + 13), data));
+
+					startService(intent);
+					bindService(intent, serviceConnection, 0);
+
+					recordingButton.setText("Start");
+					recording = false;
 				}
 			}
 		});
 
-		Button stopRecordingButton = (Button) findViewById(R.id.stop_recording_button);
-		stopRecordingButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (myTracksService != null) {
-					try {
-						myTracksService.endCurrentTrack();
-					} catch (RemoteException e) {
-						Log.e(TAG, "RemoteException", e);
-					}
-				}
+		// Alert dialog to explain that MyTracks app must be installed for statistics to work
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle("MyTracks Warning");
+		alertDialog.setMessage("You must have the Google MyTracks app installed for this page to function correctly!");
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {  
+			public void onClick(DialogInterface dialog, int which) {  
+				return;  
+			} });   
 
-				// use the MyTracks content provider to get all the tracks
-				Track tracks = myTracksProviderUtils.getLastTrack();
-				outputTextView.setText(tracks.getStatistics() + "");
-				
-				String data = outputTextView.getText().toString();
-				String mtime = "";
-				
-				for(int mtimeloc = data.indexOf("Moving Time") + 13; data.charAt(mtimeloc) != ';'; mtimeloc++){
-					mtime += (data.charAt(mtimeloc));
-				}
-				
-				movingtime.setText(mtime);
-
-				startService(intent);
-				bindService(intent, serviceConnection, 0);
-			}
-		});
+		// Display alert dialog
+		alertDialog.show();
 	}
-	
-	
+
+	/** Function to scrape the values given from the getStatistics() call to myTracks
+	 * 
+	 * @param strBeg
+	 * @param data
+	 * @return
+	 */
+	public String Scrape(int strBeg, String data) 
+	{
+		String temp = "";
+
+		/* Iterate through the value (chars) of the statistic
+		 * and append them to temp, which will be returned
+		 */
+		for( int i = strBeg; data.charAt(i) != ';'; i++ ) 
+		{
+			temp += (data.charAt(i));
+		}
+
+		return temp;
+	}
 
 	@Override
-	protected void onStart() {
+	protected void onStart() 
+	{
 		super.onStart();
 
 		// start and bind the MyTracks service
@@ -128,11 +201,13 @@ public class StatsActivity extends Activity {
 	}
 
 	@Override
-	protected void onStop() {
+	protected void onStop() 
+	{
 		super.onStop();
 
 		// unbind and stop the MyTracks service
-		if (myTracksService != null) {
+		if (myTracksService != null) 
+		{
 			unbindService(serviceConnection);
 		}
 		stopService(intent);
