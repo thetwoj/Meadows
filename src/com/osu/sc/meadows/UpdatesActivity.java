d0b14f12cc6efdf121afa6e8a6124c2f928a71a0
Michael Arnold <arnolmic@onid.orst.edu>
2011-12-01 16:23:20 -0800

    Implemented basic view for Conditions page

--- src/com/osu/sc/meadows/UpdatesActivity.java
+++ src/com/osu/sc/meadows/UpdatesActivity.java
@@ -1,19 +1,24 @@
 package com.osu.sc.meadows;
 
 import android.app.Activity;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /*
  * Place holder activity for eventual Updates features
  */
-public class UpdatesActivity extends Activity {
-
+public class UpdatesActivity extends Activity 
+{
+	
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) 
+	{
 	    super.onCreate(savedInstanceState);
-	
+	    setContentView(R.layout.updateslayout);
 	    // TODO Auto-generated method stub
 	}
-
+	
 }
