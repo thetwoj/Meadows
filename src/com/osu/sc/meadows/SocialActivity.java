8e204c4aae852acc4a1156f50ac1f15b7f00264e
Michael Arnold <arnolmic@onid.orst.edu>
2012-01-17 18:08:51 -0800

    Servery things completed

--- src/com/osu/sc/meadows/SocialActivity.java
+++ src/com/osu/sc/meadows/SocialActivity.java
@@ -3,12 +3,13 @@ package com.osu.sc.meadows;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-import MeadowsServer.Server;
-import MeadowsServer.Server;
-import MeadowsServer.User;
-import MeadowsServer.Client;
-import MeadowsServer.UsersUpdatedEvent;
-import MeadowsServer.UsersUpdatedListener;
+import server.Client;
+import server.Server;
+import server.ServerEvents;
+import server.User;
+import server.UsersUpdatedEvent;
+import server.UsersUpdatedListener;
+
 import android.view.View;
 import android.widget.EditText;
 import android.app.Activity;
@@ -19,6 +20,9 @@ import android.os.Bundle;
  */
 public class SocialActivity extends Activity 
 {
+	String text = "";
+	Client client = Client.GetInstance();
+	ServerEvents events = ServerEvents.GetInstance();
 	
 	/** Called when the activity is first created. */
     @Override
@@ -28,6 +32,36 @@ public class SocialActivity extends Activity
         
         setContentView(R.layout.sociallayout);
         
+        events.AddFriendsUpdatedListener(new UsersUpdatedListener(){
+    		public void EventFired(UsersUpdatedEvent event)
+    		{
+    			ArrayList<User> users = event.GetUsers();
+    			OnFriendsUpdated(users);
+    		}
+    	});
+    	
+    	events.AddBlockedUsersUpdatedListener(new UsersUpdatedListener(){
+    		public void EventFired(UsersUpdatedEvent event)
+    		{
+    			ArrayList<User> users = event.GetUsers();
+    			OnBlockedUsersUpdated(users);
+    		}
+    	});
+    	
+    	events.AddFriendRequestsUpdatedListener(new UsersUpdatedListener(){
+    		public void EventFired(UsersUpdatedEvent event)
+    		{
+    			ArrayList<User> users = event.GetUsers();
+    			OnFriendRequestsUpdated(users);
+    		}
+    	});
+    	
+    	events.AddLoginSuccessListener(new UsersUpdatedListener(){
+    		public void EventFired(UsersUpdatedEvent event)
+    		{
+    			OnLogin();
+    		}
+    	});        
     }
     
     /**
@@ -38,25 +72,35 @@ public class SocialActivity extends Activity
      */
     public void ButtonClicked(View view)
     {
-    	
-    	EditText text = (EditText)findViewById(R.id.testText);
-    	Server server = Server.GetServerInstance();
-    	Client client = Client.GetClientInstance();
-    	client.AddUsersUpdatedListener(new UsersUpdatedListener(){
-    		public void EventFired(UsersUpdatedEvent event)
-    		{
-    			ArrayList<User> users = event.GetUsers();
-    			OnUsersUpdated(users);
-    		}
-    	});
-    	server.RefreshUserData(25);
+    	client.Login("12345");
     }
     
-    public void OnUsersUpdated(ArrayList<User> users)
+    public void OnFriendsUpdated(ArrayList<User> users)
     {
-    	EditText text = (EditText)findViewById(R.id.testText);
-    	text.setText(Integer.toString(users.size()));
+    	EditText textBox = (EditText)findViewById(R.id.testText);
+    	text += "FriendsUpdated: "+Integer.toString(users.size()) + '\n';
+    	textBox.setText(text);
     }
     
+    public void OnLogin()
+    {
+    	EditText textBox = (EditText)findViewById(R.id.testText);
+    	text += "Logged in as " + Boolean.toString(client.GetInstance().GetGlobalVisibility()) + '\n';
+    	textBox.setText(text);
+    }
+    
+    public void OnBlockedUsersUpdated(ArrayList<User> users)
+    {
+    	EditText textBox = (EditText)findViewById(R.id.testText);
+    	text += "BlockedUsersUpdated: "+Integer.toString(users.size()) + '\n';
+    	textBox.setText(text);
+    }
+    
+    public void OnFriendRequestsUpdated(ArrayList<User> users)
+    {
+    	EditText textBox = (EditText)findViewById(R.id.testText);
+    	text += "OnFriendRequestsUpdated: "+Integer.toString(users.size()) + '\n';
+    	textBox.setText(text);
+    }
     
 }
