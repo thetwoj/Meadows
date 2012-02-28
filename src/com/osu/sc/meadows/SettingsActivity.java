12056301ebac3571801b0cd23eba6a361bece7a7
JJ Graham <thetwoj@gmail.com>
2012-02-28 12:49:37 -0800

    Forgot to add xml for preferences in last commit

---
+++ src/com/osu/sc/meadows/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.osu.sc.meadows;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity 
+{
+	String SHARED_PREFERENCES_NAME = "AppPreferences";
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {   
+		super.onCreate(savedInstanceState);
+		
+		// Set the preferences file so that it is pointing to the same file as the
+		// rest of the application
+        this.getPreferenceManager().setSharedPreferencesName(SHARED_PREFERENCES_NAME);
+     
+        // Load up the preferences xml file
+        addPreferencesFromResource(R.xml.settingslayout);
+    }
+}
