ef366ef0b8070ad7df9f7b01b13095416b9cb757
Michael Arnold <arnolmic@onid.orst.edu>
2012-04-09 02:22:25 -0700

    Minor code refactring

--- src/server/CallBack.java
+++ src/server/CallBack.java
@@ -3,10 +3,10 @@ package server;
 /*
  * This method wraps some code which is invoked upon http request completion
  */
-public class CallBack 
+public interface CallBack 
 {
 	//The callback has one method which will be called upon
 	//http request completion. This method is overwritten 
 	//to give the callback functionality.
-	public void Invoke(String result){  }
+	public void Invoke(String result);
 }
