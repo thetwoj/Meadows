53fd79147a7d2d2cf142252cb2a619fd827e5c37
Michael Arnold <arnolmic@onid.orst.edu>
2012-05-17 03:13:05 -0700

    Completed conditions parsing/event framework. Condition updates will now happen periodically

--- src/server/Conditions.java
+++ src/server/Conditions.java
@@ -1,33 +1,81 @@
 package server;
 
-import android.database.CursorJoiner.Result;
+import java.util.ArrayList;
+
 
 public class Conditions 
 {
+	ArrayList<Lift> _lifts = new ArrayList<Lift>(); 
+	ArrayList<Lot> _lots = new ArrayList<Lot>(); 
+	int _temp = Integer.MAX_VALUE;
+	
 	String _liftId = "id=\"LiftsStatus_liftsList";
+	String _lotId = "id=\"parking";
+	String _spanId = "<span";
+	String _tbodyId = "<tbody";
+	String _tbodyEndId = "</tbody";
+	String _trId = "<tr";
+	String _tdId = "<td";
+	String _tempId = "class=\"conditions-temp\"";
 	
 	public Conditions(String result)
 	{
-		_ParseResult(result);
+		try{ _ParseResult(result); }
+		catch(Exception e){}
 	}
+	
+	public ArrayList<Lift> GetLifts() { return _lifts; }
+	public ArrayList<Lot>  GetLots()  { return _lots; }
+	public int GetTemp()			  { return _temp; }
 	
 	private void _ParseResult(String result)
 	{
-		int nextLift = GetNextIndex(0, _liftId, result);;
+		//parse lifts
+		int nextLift = GetNextIndex(0, _liftId, result);
 		while(nextLift != -1)
 		{
-			
+			String name = GetNextSpanContent(nextLift, result);
+			String status = GetNextTdContent(nextLift, result);
+			_lifts.add(new Lift(name, status));
 			nextLift = GetNextIndex(nextLift, _liftId, result);
-			String lift = GetNextSpanContent(nextLift, result);
-			lift = lift + "";
 		}
 		
+		//parse parking lots
+		int nextLot = GetNextIndex(0, _lotId, result);
+		nextLot = GetNextIndex(nextLot, _tbodyId, result);
+		nextLot = GetNextIndex(nextLot, _trId, result);
+		int bodyEnd = GetNextIndex(nextLot, _tbodyEndId, result);
+		while(nextLot != -1 && nextLot < bodyEnd)
+		{
+			String name = GetNextSpanContent(nextLot, result);
+			String status = GetNextTdContent(nextLot, result);
+			_lots.add(new Lot(name, status));
+			nextLot = GetNextIndex(nextLot, _trId, result);
+		}
+		
+		//parse temp
+		int tempStart = GetNextIndex(0, _tempId, result);
+		String temp = GetNextSpanContent(tempStart, result);
+		_temp = ParseTemp(temp);
+	}
+	
+	private int ParseTemp(String temp)
+	{
+		String num = "";
+		for(int i = 0; i < temp.length(); i++)
+		{
+			char charI = temp.charAt(i);
+			if(charI >= '0' && charI <= '9')
+				num += charI;
+		}
+		return Integer.parseInt(num);
 	}
 	
 	//Starting at start, looks through 'whole' until 'part' is found,
 	//then returns the index of the beginning of the specified element
 	private int GetNextIndex(int start, String part, String whole)
 	{
+		if (start < 0) return -1;
 		//Starting at start, loop through whole looking for part
 		for(int i = start; i < whole.length(); i++)
 		{
@@ -49,7 +97,17 @@ public class Conditions
 	//Returns the content of the next span
 	private String GetNextSpanContent(int start, String whole)
 	{
-		int spanStart = GetNextIndex(start, "<span", whole);
+		return GetNextContent(start, _spanId, whole);
+	}
+	
+	private String GetNextTdContent(int start, String whole)
+	{
+		return GetNextContent(start, _tdId, whole);
+	}
+	
+	private String GetNextContent(int start, String part,String whole)
+	{
+		int spanStart = GetNextIndex(start, part, whole);
 		if(spanStart == -1)
 			return "";
 			
